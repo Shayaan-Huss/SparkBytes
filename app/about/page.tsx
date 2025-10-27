cf2c930f90e25f39eaf99d3e015d0fd382c40f41
Shan <vickyji361@gmail.com>
2025-10-27 16:01:38 -0400

    added nav bars and basic layout

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold">This is the About page</h1>
+    </main>
+  );
+}
