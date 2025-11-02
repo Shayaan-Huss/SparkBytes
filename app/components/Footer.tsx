37c973fa37697568341ac47ae122577e711278cf
NaijunWang <wnj123e@gmail.com>
2025-11-02 18:48:07 -0500

    chore: rename file (AppFooter -> Footer)

---
+++ app/components/Footer.tsx
@@ -0,0 +1,8 @@
+// components/Footer.tsx
+export default function Footer() {
+  return (
+    <footer className="w-full py-4 text-center text-sm">
+      Spark! ©2025 Created by Spark!
+    </footer>
+  );
+}
