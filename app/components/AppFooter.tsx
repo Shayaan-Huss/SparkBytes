dff143050e86a3718b52ec3967e0887aedaf07a5
NaijunWang <wnj123e@gmail.com>
2025-11-02 18:47:53 -0500

    chore: rename file (AppFooter -> Footer)

--- app/components/AppFooter.tsx
+++
@@ -1,9 +0,0 @@
-import { Layout } from "antd";
-
-const AppFooter = () => {
-  return (
-    <Layout.Footer style={{ textAlign: "center" }}>Spark! ©2025 Created by Spark!</Layout.Footer>
-  );
-};
-
-export default AppFooter;
