8a34870475b06baf66203943972616bd5cf754db
ShayaanHuss <shussain786526@gmail.com>
2025-11-12 15:00:01 -0500

    removed redundant import

--- app/createAcc/page.tsx
+++ app/createAcc/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 import Link from "next/link";
-import { useRouter } from 'next/router'
 import { useState } from "react";
 import { supabase } from '../../lib/supabaseClient'
 export default function CreateAcc() {
